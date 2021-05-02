3727748f3b19ec17b81c4acc4579a48a8bccfaf5
gustavo <gustaa1320@gmail.com>
2021-05-02 11:21:30 -0300

    Created welcome screen

---
+++ src/pages/Welcome.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+// import { Container } from './styles';
+
+export function Welcome() {
+  return (
+    <View>
+      <Text>
+        Gerencie{'\n'} 
+        suas plantas  de {'\n'}
+        forma fácil
+      </Text>
+    </View>
+  );
+}
+
+ 
