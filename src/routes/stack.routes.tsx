658d87568724d5edb9575a7670b5442f14cd72a9
gustavo <gustaa1320@gmail.com>
2021-05-02 17:43:51 -0300

    Update routes

--- src/routes/stack.routes.tsx
+++ src/routes/stack.routes.tsx
@@ -5,7 +5,7 @@ import { createStackNavigator } from '@react-navigation/stack';
 import { Welcome } from '../pages/Welcome';
 import { UserIdentification } from '../pages/UserIdentification';
 import { Confirmation } from '../pages/Confirmation';
-//import { PlantSelect } from '../pages/PlantSelect';
+import { PlantSelect } from '../pages/PlantSelect';
 
 import colors from '../styles/colors';
 
@@ -36,10 +36,10 @@ const AppRoutes: React.FC = () => (
             component = {Confirmation}
         />
 
-        {/* <stackRoutes.Screen
+        <stackRoutes.Screen
             name = "PlantSelect"
             component = {PlantSelect}
-        /> */}
+        />
     </stackRoutes.Navigator>
 )
 
